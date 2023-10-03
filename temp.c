6b40ec018ace300c73e3b03e18f7504aa091b6e8
CharlyWells <wellingtoncharlottenaaodarley@gmail.com>
2023-10-03 20:07:13 +0000

    Added new changes

--- temp.c
+++ temp.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 int main(void){
- int cel;
- int fahr;
+ float cel; //float is a datatype while cel is the varable name, float over int to avoid truncation.
+ float fahr;  //float is a datatype while fahr is the variable name
 
 for(fahr=0; fahr<=300; fahr=fahr+1)
       {
-    cel=5 *(fahr-32)/9;
-     printf("%d\t%d\n" ,fahr,cel);
+    cel = (5.0/9.0) * (fahr-32.0);
+    printf("%3.0f %6.1f\n", fahr, cel);//Prints fahr to no decimal place and cel to one decimal place.
        }
 return(0);
+}
